3ba979a34d73260393166b1ea9d71b5e530a5eb9
GuiLec <guillaumele@bam.tech>
2023-11-25 14:43:24 +0100

    chore: absolute imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,9 @@ module.exports = defineConfig({
   extends: [
     // see source: https://github.com/bamlab/react-native-project-config/blob/main/packages/eslint-plugin/lib/configs/recommended.js
     "plugin:@bam.tech/recommended",
+    "plugin:import/typescript",
   ],
+  plugins: ["import"],
   overrides: [
     {
       // config files that use CommonJS
@@ -14,4 +16,13 @@ module.exports = defineConfig({
       },
     },
   ],
+  rules: {
+    "react-native/no-raw-text": "off",
+  },
+  settings: {
+    "import/resolver": {
+      typescript: true,
+      node: true,
+    },
+  },
 });
